96872d1262de2971120cd9f1fc41fd8c2c8794ea
Yuvan Sandeep <yuvanapsandeep@gmail.com>
2026-06-08 20:21:04 +0100

    made some more letter patterns

--- main.py
+++ main.py
@@ -17,3 +17,43 @@ for i in range (n):
         print (Alphabet[alphabet_number], end = " ")
         alphabet_number = alphabet_number + 1
     print (" ")
+
+print ("===========================")
+
+word = ["P", "Y", "T", "H", "O", "N"]
+
+n = 4
+number = 0
+for i in range (n):
+    for j in range (i):
+        print (word[number], end = " ")
+        number = number + 1
+    print (" ")
+
+print ("===============================")
+
+n = 7
+number = 0
+for i in range (n):
+    for j in range (i):
+        print (word[number], end = " ")
+        number = number + 1
+    print (" ")
+    number = 0
+
+print ("============================")
+
+n = 27
+number = 0
+for i in range (n):
+    for j in range (i):
+        print (Alphabet[number], end = " ")
+        number = number + 1
+    print (" ")
+    number = 0
+
+n = 27
+number = 0
+for i in range (n):
+    print ((n-i)*" ", end = "")
+    print (2 * (i * (Alphabet[number])))
